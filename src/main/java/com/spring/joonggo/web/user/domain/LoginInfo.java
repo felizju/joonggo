878f6b6403883f9a3f6a2d0073de2387cfcccdb6
felizju1112 <felizju1112@gmail.com>
2021-06-08 12:00:53 +0900

    [21.06.08] user 구현 중

--- src/main/java/com/spring/joonggo/web/user/domain/LoginInfo.java
+++ src/main/java/com/spring/joonggo/web/user/domain/LoginInfo.java
@@ -1,4 +1,14 @@
 package com.spring.joonggo.web.user.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@ToString
 public class LoginInfo {
+
+    private String userId;
+    private String userPw;
+    private boolean autoLogin;
 }
